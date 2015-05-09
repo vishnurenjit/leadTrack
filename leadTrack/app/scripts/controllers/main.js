8bbddfd281a2f3674a57fa3baf0477fe80e525b3
Vishnu Renjit <vishnu.renjit@nibodha.com>
2015-05-09 23:08:06 +0530

    Some

--- leadTrack/app/scripts/controllers/main.js
+++ leadTrack/app/scripts/controllers/main.js
@@ -7,17 +7,33 @@
  * # MainCtrl
  * Controller of the sos00App
  */
-angular.module('sos00App').controller('MainCtrl', function ($scope, $location) {
+angular.module('sos00App').controller('MainCtrl', function ($scope, $location, BoardService) {
 
     $scope.user = {};
+    $scope.board = {lable : null, description : null}
 
   	$scope.logout = function() {
         Parse.User.logOut();
         $location.path('/login');
     };
 
+    $scope.showAddBox = function() { //alert();
+      $('#addBoard').modal('show');
+    }
+
+    $scope.addBoard = function() { //alert();
+      
+    }
 
 
+    $scope.boards = [
+      {label: "label 1", createdOn : new Date(), description : "gfvhvfjbvjvjhvhjvjhcvhcvhhjvhvjvj"},
+      {label: "label 2", createdOn : new Date(), description : "gfvhvfjbvjvjhvhjvjhcvhcvhhjvhvjvj"},
+      {label: "label 3", createdOn : new Date(), description : "gfvhvfjbvjvjhvhjvjhcvhcvhhjvhvjvj"},
+      {label: "label 4", createdOn : new Date(), description : "gfvhvfjbvjvjhvhjvjhcvhcvhhjvhvjvj"},
+      {label: "label 5", createdOn : new Date(), description : "gfvhvfjbvjvjhvhjvjhcvhcvhhjvhvjvj"},
+      {label: "label 6", createdOn : new Date(), description : "gfvhvfjbvjvjhvhjvjhcvhcvhhjvhvjvj"}
+    ]
 
 
 
