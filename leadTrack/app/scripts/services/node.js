8e451f0e51a00f852456dacfc28b9214a72f1e42
Vishnu Renjit <vishnu.renjit@nibodha.com>
2015-05-09 21:15:14 +0530

    Node service complted

--- leadTrack/app/scripts/services/node.js
+++ leadTrack/app/scripts/services/node.js
@@ -1,8 +1,52 @@
 'use strict';
 
 angular.module('sos00App')
-  .service('NodeService', function () {
+  .factory('NodeService', function () {
+  	var o = {};
+  	var Node = Parse.Object.extend("Node");
 
+  	o.createNode = function(label, description) {
+  		var node = new Node();
+  		node.set("label", label);
+  		node.set("description", description);
+  		return node;
+  	}
 
+  	o.saveNode = function(node, parent, svSuccess, svError) {
+  		node.set("createdDate", new Date());
+  		node.set("parent", parent);
 
+  		node.save(null, {
+		  success: function(node) {
+		  	svSuccess(node);
+		  },
+		  error: function(node, error) {
+		  	svError(node, error);
+		  }
+		});
+  	}
+
+  	o.getNodes = function(parent, qrySuccess) {
+  		var query = new Parse.Query(Node);
+		query.equalTo("parent", parent);
+		query.find({
+		  success: function(nodes) {
+		    qrySuccess(parent, nodes);
+		  }
+		});
+  	}
+
+  	o.getTree = function(root) {
+  		o.getNodes(root, rec);
+  	}
+
+  	var rec = function(parent, nodes) {
+  			parent.children = nodes;
+  			var i = 0;
+  			for(i = 0; i < nodes.length; i++) {
+  				o.getNodes(nodes[i], rec);
+  			}
+  		}
+
+  	return o;
   });
